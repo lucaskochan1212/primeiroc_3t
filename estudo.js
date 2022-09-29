e9c34f15aca8105b52f63479dee5eefd557dc7a0
lucaskochan1212 <110104815+lucaskochan1212@users.noreply.github.com>
2022-09-29 12:44:00 -0300

    29/09

--- estudo.js
+++ estudo.js
@@ -1,1 +1,14 @@
+let tabuada = 2
 
+function escreva(){
+    document.write(tabuada + " x 1 + = " + (tabuada))
+    document.write(tabuada + " x 2 + = " + (tabuada))
+    document.write(tabuada + " x 3 + = " + (tabuada))
+    document.write(tabuada + " x 4 + = " + (tabuada))
+    document.write(tabuada + " x 5 + = " + (tabuada))
+    document.write(tabuada + " x 6 + = " + (tabuada))
+    document.write(tabuada + " x 7 + = " + (tabuada))
+    document.write(tabuada + " x 8 + = " + (tabuada))
+    document.write(tabuada + " x 9 + = " + (tabuada))
+    document.write(tabuada + " x 10 + = " + (tabuada))
+    }
